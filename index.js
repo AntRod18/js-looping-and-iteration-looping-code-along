6885088bcf52227ffe007d004e121b56f8a4f409
AntRod18 <antrod1895@gmail.com>
2021-05-04 18:23:24 -0400

    commit tree 9058f56e365cf13efb4c5ea3425056dc387b5ed5

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let message = []
+    for (let i = 0; i < names.length; i++ ) {
+        message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return message;
+}
+
+
+function countDown(time) {
+    let i = time;
+    while (i >= 0 ) {
+        console.log(i);
+        i--;
+    }
+}
